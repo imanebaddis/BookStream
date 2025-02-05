20cf8609085f9438215da725d22582163a96121c
Imane <i.baddis@campus.uniurb.it>
2025-02-05 14:42:14 +0100

    ;

--- src/BookStream.Web/Program.cs
+++ src/BookStream.Web/Program.cs
@@ -3,17 +3,6 @@ using BookStream.Infrastructure.Common;
 using BookStream.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
-using MediatR;
-using FluentValidation;
-using BookStore.Application.Handlers;
-using BookStore.Application.Commands;
-using BookStore.Application.Validators;
-using BookStore.Domain.Interfaces;
-using BookStore.Infrastructure.Repositories;
-
-
-var builder = WebApplication.CreateBuilder(args);
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -72,78 +61,3 @@ record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
-
-
-
-// Configura i servizi per il container
-builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-        options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-        options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
-    })
-    .AddCookie()
-    .AddGoogle(options =>
-    {
-        // Legge ClientId e ClientSecret da una configurazione sicura
-        options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
-        options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
-        options.CallbackPath = "/signin-google"; // Puoi personalizzare il percorso
-    });
-
-builder.Services.AddControllersWithViews();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// Crea l'app
-var app = builder.Build();
-
-// Configura il middleware del pipeline HTTP
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts(); // Valore predefinito HSTS di 30 giorni (per HTTPS)
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-// Aggiungi l'autenticazione e autorizzazione al pipeline
-app.UseAuthentication();
-app.UseAuthorization();
-
-// Configura il routing per i controller MVC
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-// Configura le mappature degli endpoint API controller se necessario
-app.MapControllers();
-
-// Avvia l'applicazione
-app.Run();
-
-
-
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Registrazione di MediatR
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(ProcessPaymentCommandHandler).Assembly));
-
-// Registrazione dei validatori
-builder.Services.AddValidatorsFromAssembly(typeof(ProcessPaymentCommandValidator).Assembly);
-
-// Registrazione dei repository
-builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
-
-var app = builder.Build();
-
-app.Run();
